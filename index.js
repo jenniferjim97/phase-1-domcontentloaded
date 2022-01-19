c2004d17c8288633c22dc52d3bde42917005f02b
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-18 22:51:11 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  // console.log("The DOM has loaded");
+});
+ 
+const newText = document.querySelector("#text");
+newText.textContent = "This is really cool!";
